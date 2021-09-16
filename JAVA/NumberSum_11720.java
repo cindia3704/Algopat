7f54838c936a9af01f84619a261ea595d8663975
cindia3704 <cindia3704@gmail.com>
2021-09-16 20:57:04 +0900

    숫자의 합 (BOJ 11720) 자바

---
+++ JAVA/NumberSum_11720.java
@@ -0,0 +1,22 @@
+package JAVA;
+
+import java.util.Scanner;
+
+class NumberSum_11720 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        String number = scanner.next();
+        int sum = 0;
+        scanner.close();
+
+        for (int i = 0; i < length; i++) {
+            sum += number.charAt(i) - '0';
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
