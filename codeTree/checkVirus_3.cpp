7554db23929c8ca673b476143ef720c44f41cbad
cindia3704 <cindia3704@gmail.com>
2022-10-13 14:46:57 +0900

    바이러스 검사 (Code Tree 3)

---
+++ codeTree/checkVirus_3.cpp
@@ -0,0 +1,37 @@
+/*
+    작성자: 김지수
+    작성일: 2022/10/07
+    코드트리 -- 바이러스 검사 (3번)
+*/
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std; 
+
+vector<int> store;
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int storeCnt; 
+    int leader,member;
+    cin>>storeCnt;
+    for(int i=0;i<storeCnt;i++){
+        int num;
+        cin>>num;
+        store.push_back(num);
+    }
+
+    cin>>leader>>member;
+    long long testerCnt = 0;
+
+    for(int i=0;i<storeCnt;i++){
+        int peopleNum = store[i];
+        peopleNum = max(0,peopleNum-leader);
+        testerCnt+=1; 
+        if(peopleNum>0){
+            testerCnt +=ceil((double)peopleNum / (double)member);
+        }
+    }
+    cout<<testerCnt;
+}
